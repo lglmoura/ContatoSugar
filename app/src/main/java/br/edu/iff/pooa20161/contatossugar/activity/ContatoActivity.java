9f91c61186fde2132d455ee6cd1664c5a8e0b1c3
Luiz Gustavo <lglmoura@gmail.com>
2016-09-09 17:01:20 -0300

    Clique do item do Listview

--- app/src/main/java/br/edu/iff/pooa20161/contatossugar/activity/ContatoActivity.java
+++ app/src/main/java/br/edu/iff/pooa20161/contatossugar/activity/ContatoActivity.java
@@ -1,5 +1,6 @@
 package br.edu.iff.pooa20161.contatossugar.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,23 @@ public class ContatoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_contato);
+        Intent intent    = getIntent();
+        String nomep     = (String) intent.getSerializableExtra("nome");
+        String emailp    = (String) intent.getSerializableExtra("email");
+        String telefonep = (String) intent.getSerializableExtra("telefone");
+
+        EditText nome = (EditText) findViewById(R.id.etNomeContato);
+        nome.setText(nomep);
+
+        EditText email = (EditText) findViewById(R.id.etEmailContato);
+        email.setText(emailp);
+
+        EditText telefone = (EditText) findViewById(R.id.etTelefoneContato);
+        telefone.setText(telefonep);
+
+
         btsalvar = (Button) findViewById(R.id.btSalvarContato);
 
 
--- app/src/main/java/br/edu/iff/pooa20161/contatossugar/activity/ContatoLVMainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20161/contatossugar/activity/ContatoLVMainActivity.java
@@ -1,9 +1,13 @@
 package br.edu.iff.pooa20161.contatossugar.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,11 +22,25 @@ public class ContatoLVMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contato_lvmain);
 
-        ArrayList<Contato> contatos = (ArrayList) Contato.listAll(Contato.class);
+        final ArrayList<Contato> contatos = (ArrayList) Contato.listAll(Contato.class);
 
         ListView lista = (ListView) findViewById(R.id.lvContatos);
         ArrayAdapter adapter = new ContatoAdapter(this,contatos);
         lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ContatoLVMainActivity.this,ContatoActivity.class);
+
+                intent.putExtra("nome",contatos.get(i).getNome());
+                intent.putExtra("email",contatos.get(i).getEmail());
+                intent.putExtra("telefone",contatos.get(i).getTelefone());
+
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
